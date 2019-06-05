23e978ca23a8435690a408ea338752ef007eddb3
agungpambudi55 <agung.pambudi5595@gmail.com>
2019-06-05 14:20:52 +0700

    update

--- MissionHemoglobinMeter.py
+++ MissionHemoglobinMeter.py
@@ -7,17 +7,21 @@ import serial
 try:
     serCom = serial.Serial('/dev/ttyUSB0', 9600)
     
-    dataRow0 = serCom.readline()
-    dataRow1 = serCom.readline()
-    dataRow2 = serCom.readline()
-    dataRow3 = serCom.readline()
-    dataRow4 = serCom.readline()
-    dataRow5 = serCom.readline()
+    while True:
+        if serCom.inWaiting() != 0:
+            dataRow0 = serCom.readline()
+            dataRow1 = serCom.readline()
+            dataRow2 = serCom.readline()
+            dataRow3 = serCom.readline()
+            dataRow4 = serCom.readline()
+            dataRow5 = serCom.readline()
 
-    hb = dataRow2.decode('utf-8').replace('HB:','').replace('g/dL','').replace('\r\n','')
-    hct = dataRow3.decode('utf-8').replace('HCT:','').replace('%','').replace('\r\n','')
+            hb = dataRow2.decode('utf-8').replace('HB:','').replace('g/dL','').replace('\r\n','')
+            hct = dataRow3.decode('utf-8').replace('HCT:','').replace('%','').replace('\r\n','')
 
-    print('HB {} g/dL , HCT {} %'.format(hb, hct))    
+            print('HB {} g/dL , HCT {} %'.format(hb, hct))    
+                            
+            break
         
 except KeyboardInterrupt:
     print('Terminate')
